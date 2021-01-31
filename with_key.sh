597cf564b86ed70d357121a7e0e9dc675df6219f
jotzhao <jot.zhao@gmail.com>
2021-01-31 08:48:51 +0800

    Update with_key.sh

--- with_key.sh
+++ with_key.sh
@@ -11,9 +11,9 @@ if [ ! -z "$PRE_UPLOAD" ]; then
 fi
 
 echo "🚚 Uploading via scp..." &&
-scp ${INPUT_SSH_OPTIONS} ${INPUT_SCP_OPTIONS} -P "${INPUT_PORT}" -r ${INPUT_LOCAL} ubuntu@${INPUT_HOST}:"${INPUT_REMOTE}" &&
+scp ${INPUT_SSH_OPTIONS} ${INPUT_SCP_OPTIONS} -P "${INPUT_PORT}" -r ${INPUT_LOCAL} ${INPUT_USER}@${INPUT_HOST}:"${INPUT_REMOTE}" &&
 echo "🙌 Uploaded via scp";
-
+4{}
 POST_UPLOAD=${INPUT_POST_UPLOAD}
 if [ ! -z "$POST_UPLOAD" ]; then
     echo "👌 Executing post-upload script..." &&
