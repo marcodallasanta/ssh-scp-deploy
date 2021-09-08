f254055d3719e0e5d82bcc6fafa0ab46421b96ac
Marco Dalla Santa <marco.dallasanta@immobiliare.it>
2021-09-09 00:26:25 +0200

    fix(key|pass): executing log and exit in same shell but grouped up

--- with_key.sh
+++ with_key.sh
@@ -8,20 +8,20 @@ if [ ! -z "$PRE_UPLOAD" ]; then
     echo "👌 Executing pre-upload script..." &&
     ssh ${INPUT_SSH_OPTIONS} ${INPUT_USER}@${INPUT_HOST} "$INPUT_PRE_UPLOAD && exit" &&
     echo "✅ Executed pre-upload script" || 
-    echo "😢 Something went wrong during pre-upload script" && exit 1;
+    {{ echo "😢 Something went wrong during pre-upload script" && exit 1 }} ;
 fi
 
 echo "🚚 Uploading via scp..." &&
 scp ${INPUT_SSH_OPTIONS} ${INPUT_SCP_OPTIONS} -P "${INPUT_PORT}" -r ${INPUT_LOCAL} ${INPUT_USER}@${INPUT_HOST}:"${INPUT_REMOTE}" &&
 echo "🙌 Uploaded via scp" || 
-echo "😢 Something went wrong during upload" && exit 1;
+{{ echo "😢 Something went wrong during upload" && exit 1 }};
 
 POST_UPLOAD=${INPUT_POST_UPLOAD}
 if [ ! -z "$POST_UPLOAD" ]; then
     echo "👌 Executing post-upload script..." &&
     ssh ${INPUT_SSH_OPTIONS} ${INPUT_USER}@${INPUT_HOST} "$POST_UPLOAD && exit" &&
     echo "✅ Executed post-upload script" || 
-    echo "😢 Something went wrong during post-upload script" && exit 1;
+    {{ echo "😢 Something went wrong during post-upload script" && exit 1 }};
 fi
 
 echo "🎉 Done";
