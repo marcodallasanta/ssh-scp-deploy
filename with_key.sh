c04eabd1e8232981989b1c3b6bd940eeef2fc7a6
Marco Dalla Santa <marco.dallasanta@immobiliare.it>
2021-09-09 01:04:33 +0200

    fix(key): grouped up commands in same shell

--- with_key.sh
+++ with_key.sh
@@ -5,23 +5,32 @@ echo "🔐 Added ssh key";
 
 PRE_UPLOAD=${INPUT_PRE_UPLOAD}
 if [ ! -z "$PRE_UPLOAD" ]; then
-    echo "👌 Executing pre-upload script..." &&
-    ssh ${INPUT_SSH_OPTIONS} ${INPUT_USER}@${INPUT_HOST} "$INPUT_PRE_UPLOAD && exit" &&
-    echo "✅ Executed pre-upload script" || 
-    { echo "😢 Something went wrong during pre-upload script" && exit 1 } ;
+    { 
+        echo "👌 Executing pre-upload script..." &&
+        ssh ${INPUT_SSH_OPTIONS} ${INPUT_USER}@${INPUT_HOST} "$INPUT_PRE_UPLOAD && exit" &&
+        echo "✅ Executed pre-upload script"
+    } || { 
+        echo "😢 Something went wrong during pre-upload script" && exit 1
+    }
 fi
 
-echo "🚚 Uploading via scp..." &&
-scp ${INPUT_SSH_OPTIONS} ${INPUT_SCP_OPTIONS} -P "${INPUT_PORT}" -r ${INPUT_LOCAL} ${INPUT_USER}@${INPUT_HOST}:"${INPUT_REMOTE}" &&
-echo "🙌 Uploaded via scp" || 
-{ echo "😢 Something went wrong during upload" && exit 1 };
+{
+    echo "🚚 Uploading via scp..." &&
+    scp ${INPUT_SSH_OPTIONS} ${INPUT_SCP_OPTIONS} -P "${INPUT_PORT}" -r ${INPUT_LOCAL} ${INPUT_USER}@${INPUT_HOST}:"${INPUT_REMOTE}" &&
+    echo "🙌 Uploaded via scp"
+} || { 
+    echo "😢 Something went wrong during upload" && exit 1 
+}
 
 POST_UPLOAD=${INPUT_POST_UPLOAD}
 if [ ! -z "$POST_UPLOAD" ]; then
-    echo "👌 Executing post-upload script..." &&
-    ssh ${INPUT_SSH_OPTIONS} ${INPUT_USER}@${INPUT_HOST} "$POST_UPLOAD && exit" &&
-    echo "✅ Executed post-upload script" || 
-    { echo "😢 Something went wrong during post-upload script" && exit 1 };
+    {
+        echo "👌 Executing post-upload script..." &&
+        ssh ${INPUT_SSH_OPTIONS} ${INPUT_USER}@${INPUT_HOST} "$POST_UPLOAD && exit" &&
+        echo "✅ Executed post-upload script"
+    } || { 
+        echo "😢 Something went wrong during post-upload script" && exit 1 
+    }
 fi
 
 echo "🎉 Done";
