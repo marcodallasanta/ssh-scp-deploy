6df7204981beacc6b55cdf455a02e4335a675671
Marco Dalla Santa <marco.dallasanta@immobiliare.it>
2021-09-09 01:39:10 +0200

    fix(key): typo

--- with_key.sh
+++ with_key.sh
@@ -17,7 +17,7 @@ fi
 {
     echo "🚚 Uploading via scp..." &&
     scp ${INPUT_SSH_OPTIONS} ${INPUT_SCP_OPTIONS} -P "${INPUT_PORT}" -r ${INPUT_LOCAL} ${INPUT_USER}@${INPUT_HOST}:"${INPUT_REMOTE}" && 
-    echo "🙌 Uploaded via scp"
+    echo "🙌 Uploaded via scp" 
 } || { 
     echo "😢 Something went wrong during upload" && exit 1 
 }
