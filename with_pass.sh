9433396123f298d3ca0c6809d9baf4644cd9233d
James Muscat <jamesremuscat@gmail.com>
2021-10-01 21:53:10 +0100

    Honour `INPUT_PORT` in pre- and post-upload hooks.

--- with_pass.sh
+++ with_pass.sh
@@ -2,7 +2,7 @@ PRE_UPLOAD=${INPUT_PRE_UPLOAD}
 if [ ! -z "$PRE_UPLOAD" ]; then
     {
         echo "👌 Executing pre-upload script..." &&
-        sshpass -p ${PASSWORD} ssh ${INPUT_SSH_OPTIONS} ${INPUT_USER}@${INPUT_HOST} "$INPUT_PRE_UPLOAD && exit" &&
+        sshpass -p ${PASSWORD} ssh ${INPUT_SSH_OPTIONS} -p "${INPUT_PORT}" ${INPUT_USER}@${INPUT_HOST} "$INPUT_PRE_UPLOAD && exit" &&
         echo "✅ Executed pre-upload script"
     } || { 
         echo "😢 Something went wrong during pre-upload script" && exit 1
@@ -21,7 +21,7 @@ POST_UPLOAD=${INPUT_POST_UPLOAD}
 if [ ! -z "$POST_UPLOAD" ]; then
     {
         echo "👌 Executing post-upload script..." &&
-        sshpass -p ${PASSWORD} ssh ${INPUT_SSH_OPTIONS} ${INPUT_USER}@${INPUT_HOST} "$POST_UPLOAD && exit" &&
+        sshpass -p ${PASSWORD} ssh ${INPUT_SSH_OPTIONS} -p "${INPUT_PORT}" ${INPUT_USER}@${INPUT_HOST} "$POST_UPLOAD && exit" &&
         echo "✅ Executed post-upload script"
     } || {
         echo "😢 Something went wrong during post-upload script" && exit 1
